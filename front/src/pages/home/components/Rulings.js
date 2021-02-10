b19c54a4246c74ea7a33cae31ba83516d0a2f073
Daniel Gustavo Álvarez Gaitán <info@danielalvarez.com.co>
2021-02-09 21:34:43 -0500

    Button selected

--- front/src/pages/home/components/Rulings.js
+++ front/src/pages/home/components/Rulings.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Container from '@material-ui/core/Container';
 import GridList from '@material-ui/core/GridList';
 import {peopleData} from '../data/people';
@@ -6,6 +7,7 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import { common } from '@material-ui/core/colors';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -20,9 +22,27 @@ const useStyles = makeStyles((theme: Theme) =>
     icon: {
       color: 'rgba(255, 255, 255, 0.54)',
     },
+    buttonB: {
+        border: "2px solid white"
+    },
+    voteUp:{
+        backgroundColor: "#1cbbb4",
+        width: "30px",
+        minWidth: "30px",
+        paddingLeft: "1px",
+        borderRadius: 0
+    },
+    voteDown:{
+        backgroundColor: "#ffad1d",
+        width: "30px",
+        minWidth: "30px",
+        paddingLeft: "1px",
+        borderRadius: 0
+    }
   }),
 );
 export default function Rullings(){
+    const [voteOption, setVoteOption] = React.useState([]);
     const classes = useStyles();
     
     function ValidateVotes(props) {
@@ -31,6 +51,27 @@ export default function Rullings(){
         }else{
             return <span className="resumeDown"><ThumbDownIcon style={{ color: common.white,  fontSize:20 }}></ThumbDownIcon></span>;
         }
+    }
+
+    const changeStyle = (id) => {
+        console.log(id);
+        const ids = id.split('-');
+        if (ids[2] == 1) {
+            let element = document.getElementById("vote-"+ids[1]+"-2");
+            ReactDOM.findDOMNode(element).style.border = "none";
+        } else if(ids[2] == 2){
+            let element = document.getElementById("vote-"+ids[1]+"-1");
+            ReactDOM.findDOMNode(element).style.border = "none";
+        }
+        
+        let element = document.getElementById(id);
+        ReactDOM.findDOMNode(element).style.border = "2px solid white";
+    }
+
+    const Vote = () => {
+        console.log("voteOption:");
+        
+        console.log(voteOption);
     }
     return <div id="rullings">
         <Container maxWidth="md">
@@ -45,15 +86,21 @@ export default function Rullings(){
                                     {person.name}</h4>
                                 <p>{person.description}</p>
                                 <div className="votePerson">
-                                    <button className="voteUp">
+                                    <Button id={'vote-'+person.id+'-'+1} className={classes.voteUp} onClick={(e)=>{
+                                        setVoteOption([person.id, 1]);
+                                        changeStyle('vote-'+person.id+'-'+1);
+                                    }}>
                                         <ThumbUpAltIcon style={{ color: common.white, fontSize:20 }}></ThumbUpAltIcon>  
-                                    </button>
-                                    <button className="voteDown">
+                                    </Button >
+                                    <Button id={'vote-'+person.id+'-'+2} className={classes.voteDown} onClick={()=>{
+                                        setVoteOption([person.id, 2]);
+                                        changeStyle('vote-'+person.id+'-'+2);
+                                    }}>
                                         <ThumbDownIcon style={{ color: common.white,  fontSize:20 }}></ThumbDownIcon>
-                                    </button>
-                                    <button className="vote">
+                                    </Button>
+                                    <Button className="vote" onClick={Vote}>
                                         Vote now
-                                    </button>
+                                    </Button>
                                 </div>
                             </div>
                             <div className="voteResults">
